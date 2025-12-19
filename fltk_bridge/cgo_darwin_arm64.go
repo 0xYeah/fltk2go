8e8d57baef6aed70a1f4951982b826cef280dccf
0xYeah <wmyeah017@gmail.com>
2025-12-19 08:29:34 +0800

    add mac lib binary

---
+++ fltk_bridge/cgo_darwin_arm64.go
@@ -0,0 +1,8 @@
+//go:build darwin && arm64
+
+package fltk_bridge
+
+// #cgo darwin,arm64 CPPFLAGS: -I${SRCDIR}/../lib/darwin/arm64 -I${SRCDIR}/../lib/include -I${SRCDIR}/../lib/include/FL/images
+// #cgo darwin,arm64 CXXFLAGS: -std=c++11
+// #cgo darwin,arm64 LDFLAGS: ${SRCDIR}/../lib/darwin/arm64/libfltk_images.a ${SRCDIR}/../lib/darwin/arm64/libfltk_jpeg.a ${SRCDIR}/../lib/darwin/arm64/libfltk_png.a ${SRCDIR}/../lib/darwin/arm64/libfltk_z.a ${SRCDIR}/../lib/darwin/arm64/libfltk_gl.a ${SRCDIR}/../lib/darwin/arm64/libfltk_forms.a ${SRCDIR}/../lib/darwin/arm64/libfltk.a -framework Cocoa -framework Carbon -framework ApplicationServices -framework CoreGraphics -framework CoreText -framework QuartzCore -framework IOKit -framework OpenGL
+import "C"
