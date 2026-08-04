454bd44db2efff99f0ac2a51000974725e399215
yerikokay <129245892+yerikokay@users.noreply.github.com>
2026-08-04 18:15:34 +0000

    build: upgrade Linux FLTK artifacts to 1.4.5

--- config/config.go
+++ config/config.go
@@ -3,6 +3,6 @@ package config
 const (
 	ProjectName         = "fltk2go"
 	ProjectVersion      = "v0.0.33"
-	FLTKPreBuildVersion = "release-1.4.4"
+	FLTKPreBuildVersion = "release-1.4.5"
 	FLTKCppStandard     = "c++11"
 )
