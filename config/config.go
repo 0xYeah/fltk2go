d278b69fb1e15d231d38bdc5913cee2dff87fd33
yerikokay <129245892+yerikokay@users.noreply.github.com>
2026-08-04 19:18:05 +0000

    Release:--: v0.0.34_2026-08-04_19:18:05_UTC

--- config/config.go
+++ config/config.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	ProjectName         = "fltk2go"
-	ProjectVersion      = "v0.0.33"
+	ProjectVersion      = "v0.0.34"
 	FLTKPreBuildVersion = "release-1.4.5"
 	FLTKCppStandard     = "c++11"
 )
